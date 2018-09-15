ad66caf5800d39f06678a7f685cd31d971a1d33c
vlad <vee.short.5014@gmail.com>
2018-09-15 17:05:17 +0300

    first chrome app commit

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 'use strict';
 
+chrome.app.runtime.onLaunched.addListener(() => {
+  console.log('launched');
+  chrome.app.window.create('index.html', {
+    'outerBounds': {
+      'width': 400,
+      'height': 500
+    }
+  });
+  init();
+});
+
 let formEl, emailEl, passwordEl;
 
 let authToken = '';
@@ -15,18 +26,15 @@ const rest = {
   }
 };
 
-window.onload = init;
-
 function checkToken() {
-  let tokenStorageVal = localStorage.getItem('auth_token');
-  if (tokenStorageVal !== null) {
-    authToken = tokenStorageVal;
+  console.log('checking auth token...');
+  chrome.storage.local.get(['auth_token'], (res) => {
+    authToken = res;
     formEl.style = 'display: none';
-  }
+  });
 }
 
-function submitLoginForm(event) {
-  console.log('submit event:', event);
+function submitLoginForm() {
   const formData = new FormData();
   formData.append('email', emailEl.value.trim());
   formData.append('password', passwordEl.value.trim());
@@ -40,7 +48,7 @@ function submitLoginForm(event) {
   }).then(res => {
     res.json().then(res => {
       authToken = res.user.auth_token;
-      localStorage.setItem('auth_token', authToken);
+      chrome.storage.local.set({auth_token: authToken});
     }).catch(err => console.error(err));
   }).catch(err => console.error(err));
 }
@@ -65,8 +73,11 @@ function getTodaysHours() {
 }
 
 function init() {
+  console.log('init()');
   formEl = document.querySelector('#form');
   emailEl = document.querySelector('#email-inp');
   passwordEl = document.querySelector('#pass-inp');
   checkToken();
+  document.querySelector('#submit-btn').addEventListener('click', submitLoginForm);
+  document.querySelector('#get-h-btn').addEventListener('click', getTodaysHours);
 }
